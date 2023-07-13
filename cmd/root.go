aa7b29fa694cc55b75dac2b61034ea9b8d15f2a7
Ryu Tanabe <bellx2@gmail.com>
2023-07-13 22:39:08 +0900

    Add version

--- cmd/root.go
+++ cmd/root.go
@@ -60,5 +60,6 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
+	rootCmd.Version = version
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
