28e573c4d989c592b73f3bc40b2eeafcf487bbbd
Ryu Tanabe <bellx2@gmail.com>
2023-07-13 21:56:03 +0900

    Add Version

--- cmd/root.go
+++ cmd/root.go
@@ -27,7 +27,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
+var version string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -62,7 +62,6 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
+	rootCmd.Version = version
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
